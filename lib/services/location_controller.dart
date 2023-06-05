cd05abda92d017694668d474614c84e0d57bfcd3
Koby Mashiach <koby9779@gmail.com>
2023-06-05 20:41:39 +0300

    add

--- lib/services/location_controller.dart
+++ lib/services/location_controller.dart
@@ -1,29 +1,29 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-import 'package:google_maps_webservice/src/places.dart';
+// import 'package:flutter/material.dart';
+// import 'package:geocoding/geocoding.dart';
+// import 'package:get/get.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:google_maps_webservice/src/places.dart';
 
-class LocationController extends GetxController {
-  Placemark _pickPlaceMark = Placemark();
-  Placemark get pickPlaceMark => _pickPlaceMark;
+// class LocationController extends GetxController {
+//   Placemark _pickPlaceMark = Placemark();
+//   Placemark get pickPlaceMark => _pickPlaceMark;
 
-  List<Prediction> _predictionList = [];
+//   List<Prediction> _predictionList = [];
 
-  Future<List<Prediction>> searchLocation(
-      BuildContext context, String text) async {
-    if (text != null && text.isNotEmpty) {
-      http.Response response = await getLocationDate(text);
-      var data = jsonDecode(response.body.toString());
-      print("my status is " + data["status"]);
-      if (data['status'] == 'OK') {
-        _predictionList = [];
-        data['predictions'].forEach((prediction) =>
-            _predictionList.add(Prediction.fromJson(prediction)));
-      } else {}
-    }
-    return _predictionList;
-  }
-}
+//   Future<List<Prediction>> searchLocation(
+//       BuildContext context, String text) async {
+//     if (text != null && text.isNotEmpty) {
+//       http.Response response = await getLocationDate(text);
+//       var data = jsonDecode(response.body.toString());
+//       print("my status is " + data["status"]);
+//       if (data['status'] == 'OK') {
+//         _predictionList = [];
+//         data['predictions'].forEach((prediction) =>
+//             _predictionList.add(Prediction.fromJson(prediction)));
+//       } else {}
+//     }
+//     return _predictionList;
+//   }
+// }
